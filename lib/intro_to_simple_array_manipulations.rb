cc7d3ecfbd4bc20e8bb7d71fa2261a9e6a485400
derek chen <derek.g.chen@gmail.com>
2018-03-21 17:30:34 +0000

    automatically backed up by learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,28 @@
+def using_push (array, element)
+  array << element
+end
+
+
+def using_unshift (array, element)
+  array.unshift(element)
+  
+end
+
+
+def using_pop (array)
+  array.pop()
+end
+
+
+def pop_with_args (array)
+  array1 =[]
+  array1 << array.pop()
+  array1 << array.pop()
+  return array1
+  
+end
+
+
+def using_push (array, element)
+  
+end
